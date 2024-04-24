e9c645db30ba882b95e9738093229b11bfd42505
Benjamin Zwicker <37882525+benjaminzwicker@users.noreply.github.com>
2024-04-24 20:54:47 -0300

    Add routing, header component, pages, MUI styles (#3)

---
+++ src/pages/Calculator.tsx
@@ -0,0 +1,11 @@
+import MahandHeader from '../components/MahandHeader'
+
+const Calculator: React.FC = () => {
+    return (
+        <div>
+            <MahandHeader>Mahjong Hand Calculator</MahandHeader>
+        </div>
+    )
+}
+
+export default Calculator
---
+++ src/pages/Feedback.tsx
@@ -0,0 +1,11 @@
+import MahandHeader from '../components/MahandHeader'
+
+const Feedback: React.FC = () => {
+    return (
+        <div>
+            <MahandHeader>Feedback</MahandHeader>
+        </div>
+    )
+}
+
+export default Feedback
